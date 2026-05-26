8f648f528698d3aefdf4c8d30b946135223e99b2
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-05-26 16:58:16 +0300

    add UserProfileController for user profile management with show, update, and destroy methods

---
+++ backend_development/app/Http/Controllers/User/UserProfileController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Helpers\ApiResponse;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\User\UpdateUserProfileRequest;
+use App\Http\Resources\UserProfileResource;
+use Illuminate\Http\Request;
+
+class UserProfileController extends Controller
+{
+
+    public function show(Request $request)
+    {
+        $user = $request->user();
+
+        return ApiResponse::success(
+            'User profile retrieved successfully.',
+            [
+                'user' => new UserProfileResource($user),
+            ]
+        );
+    }
+
+
+
+    public function update(UpdateUserProfileRequest $request)
+    {
+        $user = $request->user();
+
+        $user->update($request->validated());
+
+        return ApiResponse::success(
+            'User profile updated successfully.',
+            [
+                'user' => new UserProfileResource($user->fresh()),
+            ]
+        );
+    }
+
+    
+
+    public function destroy(Request $request)
+    {
+        $user = $request->user();
+
+        $user->tokens()->delete();
+        return ApiResponse::success(
+            'User profile deleted successfully.'
+        );
+    }
+}
