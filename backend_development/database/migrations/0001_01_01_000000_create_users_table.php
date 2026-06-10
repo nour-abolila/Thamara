bb939620677724ad1ffd0e9a48f1f26d2a487db7
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2026-06-11 00:44:05 +0300

    feat: update users table to allow nullable password and add social_accounts migration

--- backend_development/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend_development/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('phone_number')->nullable();
